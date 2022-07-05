6b74097bf7762baa4c9f6dcc91d6d088744414f1
Gabriel Vieira <gabriel.vieira@ECO.LAN>
2022-07-05 17:02:28 -0300

    feat: Finalizado seção 8

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,6 @@ app_name = 'recipes'
 # Register your models here.
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
